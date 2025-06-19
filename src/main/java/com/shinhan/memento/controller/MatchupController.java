8ceddb24e38f7046f70111fdb5650197f428a26f
jungeun <kje2066@naver.com>
2025-06-19 14:13:29 +0900

    chore: 디렉토리 구조 수정

--- src/main/java/com/shinhan/memento/controller/MatchupController.java
+++ src/main/java/com/shinhan/memento/controller/MatchupController.java
@@ -2,18 +2,19 @@ package com.shinhan.memento.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MatchupController {
 	
 	@GetMapping("/matchupDetail")
 	public String f1() {
-		return "matchupDetail";
+		return "/matchup/matchupDetail";
 	}
 	
 	@GetMapping("/matchupDetailLeader")
 	public String f2() {
-		return "matchupDetailLeader";
+		return "/matchup/matchupDetailLeader";
 	}
 
 }
