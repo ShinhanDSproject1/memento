d38fddcb0e618c6acc3bf4606c9a34613eb11b9b
danya94-2 <sin_sit@naver.com>
2025-06-23 12:33:06 +0900

    feat : 중간병합
    중간병합
    
    Resolves:
    Ref:
    Related to: #9

--- src/main/webapp/resources/static/js/sidebar.js
+++ src/main/webapp/resources/static/js/sidebar.js
@@ -1,3 +1,59 @@
+function s_click(word) {
+  $('.menu-item').removeClass('active');
+
+  switch (word) {
+    case 'dashboard':
+      $(".div3").addClass('active');
+      localStorage.setItem('activeMenu', 'dashboard');
+      location.href = basePath + '/mypage/dashBoard';
+      break;
+    case 'cash':
+      $(".div4").addClass('active');
+      localStorage.setItem('activeMenu', 'cash');
+      alert('캐시 충전 준비중');
+      break;
+    case 'pay':
+      $(".div5").addClass('active');
+      localStorage.setItem('activeMenu', 'pay');
+      location.href = basePath + '/mypage/page3';
+      break;
+    case 'spark':
+      $(".spark-test").addClass('active');
+      localStorage.setItem('activeMenu', 'spark');
+      alert('Spark Test 준비중');
+      break;
+    case 'm_test':
+      $(".div6").addClass('active');
+      localStorage.setItem('activeMenu', 'm_test');
+      alert('멘토 자격시험 준비중');
+      break;
+  }
+}
+
+$(document).ready(function() {
+  var activeMenu = localStorage.getItem('activeMenu');
+  if (activeMenu) {
+    $('.menu-item').removeClass('active');
+    switch (activeMenu) {
+      case 'dashboard':
+        $(".div3").addClass('active');
+        break;
+      case 'cash':
+        $(".div4").addClass('active');
+        break;
+      case 'pay':
+        $(".div5").addClass('active');
+        break;
+      case 'spark':
+        $(".spark-test").addClass('active');
+        break;
+      case 'm_test':
+        $(".div6").addClass('active');
+        break;
+    }
+  }
+});
+
 // DOM 로딩 후 스크린에 initial page 로드
 $(function () {
   $('#screen').load(initialPage);
