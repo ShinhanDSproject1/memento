a1990ec01791f9d4ac704ee6636d8ed4c8fb292b
choidahee11 <jedihappy11@gmail.com>
2025-06-27 09:05:42 +0900

    Controller 오류 메세지 수정했습니다

--- src/main/java/com/shinhan/memento/controller/MentosController.java
+++ src/main/java/com/shinhan/memento/controller/MentosController.java
@@ -1,7 +1,7 @@
 package com.shinhan.memento.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +11,7 @@ import com.shinhan.memento.common.exception.MentosException;
 import com.shinhan.memento.common.response.BaseResponse;
 import com.shinhan.memento.common.response.status.BaseExceptionResponseStatus;
 import com.shinhan.memento.dto.CreateMentosDTO;
+import com.shinhan.memento.dto.MentosDTO;
 import com.shinhan.memento.service.MentosService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -60,12 +61,14 @@ public class MentosController {
 
 	@PostMapping("/creatementos")
 	public BaseResponse<Void> createMento(@RequestBody CreateMentosDTO dto) {
-		log.info("[MentosController.createMento]");
+		log.info("MentosController.createMento");
 		boolean result = mentosService.registerMento(dto);
 		if (!result) {
-			throw new MentosException(BaseExceptionResponseStatus.CANNOT_FOUND_MEMBER);
+			throw new MentosException(BaseExceptionResponseStatus.CANNOT_CREATE_MENTOS);
 		}
 		return new BaseResponse<>(null);
 	}
+	
+
 
 }
