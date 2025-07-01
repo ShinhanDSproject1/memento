1ecb9b7d4f76e88012c1d8713fb7075c342aaa9c
Josangho99 <josangho99@gmail.com>
2025-07-01 18:56:52 +0900

    feat: dto 및 mapper select 수정중

---
+++ src/main/java/com/shinhan/memento/dto/MyPaymentListResponseDTO.java
@@ -0,0 +1,20 @@
+package com.shinhan.memento.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class MyPaymentListResponseDTO {
+	private Integer orderId;
+	private Integer amount;
+	private Integer matchupId;
+	private Integer mentosId;
+	private Integer keepgoingId;
+	private String paymentStatus;
+	private String matchupTitle;
+	private String mentosTitle;
+	private String keepgoingName;
+}
