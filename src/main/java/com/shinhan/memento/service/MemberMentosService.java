0844fbece0ac60ea5aa5e575a8d165232b2a046b
jungeun <kje2066@naver.com>
2025-07-02 11:35:18 +0900

    feat: cancelJoinMentos 구현

--- src/main/java/com/shinhan/memento/service/MemberMentosService.java
+++ src/main/java/com/shinhan/memento/service/MemberMentosService.java
@@ -6,6 +6,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.shinhan.memento.dto.JoinMentosDTO;
 import com.shinhan.memento.mapper.MemberMentosMapper;
+import com.shinhan.memento.model.MemberMentos;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -21,7 +22,22 @@ public class MemberMentosService {
 	 */
 	@Transactional
 	public int joinMentos(JoinMentosDTO joinMentoDto) {
-		log.info("[MentosService.joinMentos]");
+		log.info("[MemberMentosService.joinMentos]");
 		return memberMentosMapper.joinMentos(joinMentoDto);
+	}
+	
+	// memberId, mentosId 로 유효한 member_mentos 가 있는지 확인 
+	public MemberMentos checkValidMemberMentos(JoinMentosDTO joinMentoDto) {
+		log.info("[MemberMentosService.checkValidMemberMentos]");
+		return memberMentosMapper.findMemberMentosById(joinMentoDto);
+	}
+	
+	/**
+	 * 멘토스 참여 취소(신청 취소하기)
+	 */
+	@Transactional
+	public int cancelJoinMentos(int memberMentosId) {
+		log.info("[MemberMentosService.cancelJoinMentos]");
+		return memberMentosMapper.cancelJoinMentos(memberMentosId);
 	}
 }
