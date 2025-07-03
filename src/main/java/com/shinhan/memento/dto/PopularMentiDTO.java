8caae4f50cc418ca2e96f8dfc95796442550a6c4
77 <77@DESKTOP-IUR107D>
2025-07-04 01:33:57 +0900

    feat: 메인페이지 랭킹 기능 구현 중

--- src/main/java/com/shinhan/memento/dto/PopularMentiDTO.java
+++ src/main/java/com/shinhan/memento/dto/PopularMentiDTO.java
@@ -1,10 +1,14 @@
 package com.shinhan.memento.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor      
+@AllArgsConstructor
 public class PopularMentiDTO {
 	private int memberId;
     private String nickname;
