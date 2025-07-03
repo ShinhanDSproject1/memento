af504c54e940d1f55263e155674a80980057108e
jungeun <kje2066@naver.com>
2025-07-03 14:47:11 +0900

    feat: 멘토 상세보기 페이지 중 진행한 멘토스 보기를 위한 dto 작성

---
+++ src/main/java/com/shinhan/memento/dto/mentoDetail/MentoDetailClassDTO.java
@@ -0,0 +1,22 @@
+package com.shinhan.memento.dto.mentoDetail;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class MentoDetailClassDTO {
+	private String mentosImg;
+	private String title;
+	private String mentoName;
+	private String userType;
+	private String startDay;
+	private String endDay;
+	private String startTime;
+	private String endTime;
+	private String selectedDays;
+	private String region; // regionGroup + regionSubgroup
+	private int price;
+}
