ff2d522c00a256d4be71f32719e5ec737af801fc
jungeun <kje2066@naver.com>
2025-07-08 13:07:44 +0900

    fix: 소개글에 Html 태그 그대로 들어오는거 수정

--- src/main/webapp/resources/js/mentos/mentosDetail.js
+++ src/main/webapp/resources/js/mentos/mentosDetail.js
@@ -34,14 +34,14 @@ document.addEventListener('DOMContentLoaded', function () {
 	        $('.category').text(`#${mentos.categoryName}`);
 			$('.language').text(`#${mentos.languageName}`);
 	        $('.class-title').text(mentos.title);
-	        $('.class-description').text(mentos.simpleContent);
+			$('.class-description').text(mentos.simpleContent);
 	        $('#currentMemberCnt').text(mentos.currentMemberCnt);
 	        $('#startDay').text(mentos.startDay);
 	        $('#endDay').text(mentos.endDay);
 	        $('#startTime').text(mentos.startTime);
 			$('#endTime').text(mentos.endTime);
 
-			$('.mentos-class-image').attr('src',`${cpath}/resources/uploadImage/${mentos.image}`);
+			$('.mentos-class-image').attr('src',`${mentos.image}`);
 			$('.location-address').text(mentos.place);
 			const rawDays = mentos.selectedDays;
 			const selectedCodes = rawDays.split(',');
@@ -53,7 +53,7 @@ document.addEventListener('DOMContentLoaded', function () {
 			$('#times').text(mentos.times);
 			$('#place').text(mentos.place);
 			$('#price').text('₩'+mentos.price);
-			$('.class-summary-section').text(mentos.content); 
+			$('.class-summary-section').html(mentos.content); 
 			$('.partner-name1').text(mentos.matchTypeNameFirst);
 			$('.partner-name2').text(mentos.matchTypeNameSecond);
 			$('.partner-name3').text(mentos.matchTypeNameThird);
